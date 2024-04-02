5e87e0f917cea997d8a0fc7082694a90617ed182
tjsiv <tjsivelsjr@gmail.com>
2024-04-01 21:55:36 -0700

    first commit

--- public/index.js
+++ public/index.js
@@ -9,14 +9,30 @@ async function main(){
     
     let countValue = result.value;
 
+    async function updateCounter(newValue){
+        let response = await fetch('http://localhost:9001/counter', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'value': newValue
+            })
+        })
+        let result = await response.json()
+        console.log(result)
+    }
+
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        updateCounter(countValue)
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        updateCounter(countValue)
     }
 
     incrementButton.addEventListener('click', increment);
